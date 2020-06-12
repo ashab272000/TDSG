df313e5abe02742b6ecd97bce059f8425de24fa1
Mohammed Ashab Uddin <mu15@hw.ac.uk>
2020-06-12 08:21:43 +0400

    Added static var "gameobject" and "nextId"

--- src/GameEngine/GameObject/game_object.ts
+++ src/GameEngine/GameObject/game_object.ts
@@ -1,16 +1,33 @@
 import { Transform } from "./transform";
 
 export class GameObject {
-    private _id: number;
+    //saves all the GameObject instances
+    public static gameObjects : GameObject[];
+    //static number, which will provide id
+    //for every gameobject starting from 0
+    public static nextId
+    //id of the gameobject
+    private id: number;
+    //transform of the gameobject
+    //deals with the position and rotation of gameobject
     public transform: Transform;
+    //children of the gameobject
     public children: GameObject[];
+    //isEnabled will allow us to have a gameobject in the world
+    //but not active
     public isEnabled: boolean;
 
     constructor() {
-        this._id = 0;
+        this.id = GameObject.nextId;
         this.transform = new Transform();
         this.children = [];
         this.isEnabled = true;
+
+        //add the instance of a GameObject
+        //to the static gameobject
+        GameObject.gameObjects.push(this);
+        //increment the nextId
+        GameObject.nextId++;
     }
 
     addChild(object: GameObject) {
