8d6ed9b03b0069368d560e9d2c4d1759c6b8faaa
Mohammed Ashab Uddin <mu15@hw.ac.uk>
2020-06-12 08:21:00 +0400

    added cellscount

--- src/GameEngine/WorldClass/cell.ts
+++ src/GameEngine/WorldClass/cell.ts
@@ -1,18 +1,30 @@
 
-class Cell {
+export class Cell {
 
-    private _id: number;
-    private _occupiedId = -1;
+    //id of the cell
+    private id: number;
+    //id of the object that occupied the cell
+    private occupiedId = -1;
+    //static number
+    //this keeps count of all the cells created
+    //when a cell is created, cellsCount is incremented
+    static cellsCount : number = 0;
 
-    constructor(id: number){
-        this._id = id;
+    constructor(){
+        //this two lines of code will allow us to have 
+        //incremented id for each of the cells created
+        //first cell will be 0,
+        //second cell will be 1,
+        //nth cell will be n-1,
+        this.id = Cell.cellsCount;
+        Cell.cellsCount++;
     }
 
-    public occupy(){
-        
+    public occupy(occupiedId : number){
+        this.occupiedId = occupiedId;
     }
 
     public deOccupy(){
-        this._occupiedId = -1;
+        this.occupiedId = -1;
     }
 }
