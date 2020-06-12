0a5f745a365693bc58e72a17aa075943c2da11f5
Mohammed Ashab Uddin <mu15@hw.ac.uk>
2020-06-12 08:19:45 +0400

    added worldgenerator to game_runner,

--- src/GameEngine/game_runner.ts
+++ src/GameEngine/game_runner.ts
@@ -1,20 +1,40 @@
 
 import { GameObject } from './GameObject/game_object';
+import { WorldGenerator } from './WorldClass/world_generator';
 
 export class GameRunner {
 
+    //gameobject is an array of all the gameobject instances in the world
     private gameObjects : GameObject[];
+    private worldGenerator : WorldGenerator;
 
-    init(){
+
+    constructor( worldSize = 25){
+        //gets all the gameObject instances
+        this.gameObjects = GameObject.gameObjects;
+        //Creates a worldgenerator
+        this.worldGenerator = new WorldGenerator(worldSize);
+        this.init();
+        this.update();
+
+    }
+
+    private init(){
+        //inititalizes all the gameobjects
         this.gameObjects.forEach(element => {
             element.init()   
         });
     }
 
-    update(){
-        this.gameObjects.forEach(element => {
-            element.update()   
-        });
+    private update(){
+        //40ms = 25fps
+        //This function runs an interval throughout the game
+        //it updates all the gameobjects with their update functions
+        setInterval( () => {
+            this.gameObjects.forEach(element => {
+                element.update()   
+            });
+        }, 40)
     }
 
 }
