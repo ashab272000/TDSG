2da37befec58f4beb3570cac8cbeebed5194b2d0
Mohammed Ashab Uddin <mu15@hw.ac.uk>
2020-06-20 18:00:23 +0400

    removed movement object

--- src/GameEngine/GameObject/transform.ts
+++ src/GameEngine/GameObject/transform.ts
@@ -4,7 +4,6 @@ import { World } from "../WorldClass/world";
 export class Transform {
     public position: Position;
     public rotation: number;
-    private cellId: number;
 
     constructor() {
         this.position = new Position();
@@ -14,28 +13,10 @@ export class Transform {
     public translate(x: number, y: number) {
         this.position.x += x;
         this.position.y += y;
-        this.cellId = this.calculateCellId();
     }
 
     public rotate(degree: number) {
         this.rotation += degree;
     }
-
-    public getCellId(){
-        return this.cellId;
-    }
-
-    private calculateCellId(){
-        //calculate the cell where the transform is
-        //by using the x and y values
-        
-        let worldSize = World.getInstance().getSize();
-        let y = Math.floor(this.position.y)  * worldSize;
-        let cellId = y + Math.floor(this.position.x);
-
-        return cellId;
-    }
-
-
 
 }
